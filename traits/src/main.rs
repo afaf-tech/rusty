e46e8a09a7f12efa311c6b8db00f48578644d442
afaf-tech <fatihalfikri5@gmail.com>
2022-02-26 16:00:32 +0700

    trait default implementation

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -5,8 +5,12 @@ pub struct NewsArticle {
 }
 
 impl Summary for NewsArticle {
-    fn summarize(&self) -> String{
-        format!("{}, by {}", self.headline, self.author)
+    // fn summarize(&self) -> String{
+    //     format!("{}, by {}", self.headline, self.author)
+    // }
+
+    fn summarize_author(&self) -> String{
+        format!("@{}", self.author)
     }
 }
 
@@ -18,13 +22,23 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
+
+    fn summarize_author(&self) -> String{
+        format!("@{}", self.username)
+    }
+
     fn summarize(&self) -> String{
         format!("{}, by {}", self.username, self.content)
     }
 }
 
 pub trait Summary {
-    fn summarize(&self) -> String;
+
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
 }
 
 fn main() {
