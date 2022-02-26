cadeeae0faeb2da157e8a2516e67ed65ca6bc36a
afaf-tech <fatihalfikri5@gmail.com>
2022-02-26 15:45:01 +0700

    implementing traits

---
+++ traits/src/main.rs
@@ -0,0 +1,47 @@
+pub struct NewsArticle { 
+    pub author: String,
+    pub headline: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String{
+        format!("{}, by {}", self.headline, self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String{
+        format!("{}, by {}", self.username, self.content)
+    }
+}
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+fn main() {
+    let tweet = Tweet{
+        username: String::from("@johndoe"),
+        content: String::from("Hello world!"),
+        reply: false,
+        retweet: false,
+    };
+
+    let article = NewsArticle {
+        author: String::from("John Doe"),
+        headline: String::from("The Sky is Falling!"),
+        content: String::from("The sky is not actually falling"),
+    };
+
+    println!("Tweet summary : {}", tweet.summarize());
+    println!("Article summary : {}", article.summarize());
+
+}
