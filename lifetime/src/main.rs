fd14b6a3c861e4b81d25378e7632487625789186
afaf-tech <fatihalfikri5@gmail.com>
2022-03-06 05:03:32 +0700

    lifetime generical Lifetime Annotations

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,5 +1,30 @@
 fn main() {
+    // let string1 = String::from("abcd");
+    // let string2 = String::from("xyz");
 
+    // let result = longest( string1.as_str(), string2.as_str() );
+    // println!("The longest string i {}", result);
+
+    let result;
+    let string1 = String::from("abcd");
+    { 
+        let string2 = String::from("xyz");
+        result = longest( string1.as_str(), string2.as_str() );
+    }
+    println!("The longest string i {}", result);
+
+
+}
+
+// &i32     // a reference
+// &'a i32  // a reference with an explicit lifetime
+// &'a mut i32 // a mutable reference with an explicit lifetime
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x 
+    }else {
+        y
+    }
 }
 
 
