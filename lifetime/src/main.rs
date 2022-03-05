1df3c4cc0c827799e135d6078736c80d2576cf22
afaf-tech <fatihalfikri5@gmail.com>
2022-03-06 05:05:34 +0700

    lifetime thinking

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -19,12 +19,9 @@ fn main() {
 // &i32     // a reference
 // &'a i32  // a reference with an explicit lifetime
 // &'a mut i32 // a mutable reference with an explicit lifetime
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x 
-    }else {
-        y
-    }
+fn longest<'a>(x: &'a str, y: &str) -> String {
+    let result = String::from("Very long string result");
+    result
 }
 
 
