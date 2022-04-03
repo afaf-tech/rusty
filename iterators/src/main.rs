295670d7ad2e0cc128eaad6ebfd380cfeffd8d0e
afaf-tech <fatihalfikri5@gmail.com>
2022-04-03 07:58:55 +0700

    creating our own iterators

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,10 +1,10 @@
-pub trait Iterator {
-    type Item;
+// pub trait Iterator {
+//     type Item;
 
-    fn next(&mut self) -> Option<Self::Item>;
+//     fn next(&mut self) -> Option<Self::Item>;
 
-    // methods with default implementations elided
-}
+//     // methods with default implementations elided
+// }
 
 #[test]
 fn iterator_demonstration() {
@@ -94,4 +94,51 @@ mod tests {
         ];
         assert_eq!(in_my_size, expected_shoes);
     }
+}
+
+
+// Creating our own iterators
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter {count: 0}
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+}
+
+#[test]
+fn using_other_iterator_trait_methods(){
+    let sum: u32= Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b )| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    assert_eq!(18, sum);
 }
