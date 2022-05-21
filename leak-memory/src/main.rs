3948fe13ec5b914f0de9446042ddbe4185ea8230
afaf-tech <fatihalfikri5@gmail.com>
2022-05-21 19:07:23 +0700

    feat: creating reference cycle

---
+++ leak-memory/src/main.rs
@@ -0,0 +1,56 @@
+// Reference cycle can leak memory (Rc<T> RefCell<T>)
+
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List{
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item )=> Some(item),
+            Nil => None,
+        }
+    }
+}
+
+
+fn main() {
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    /* 
+        We modify a so it points to b instead of Nil, creating a cycle. 
+        We do that by using the tail method to get a reference to the RefCell<Rc<List>> in a, which we put in the variable link. 
+        Then we use the borrow_mut method on the RefCell<Rc<List>> to change the value inside from an Rc<List> that holds a Nil value to the Rc<List> in b.
+    */
+    if let Some(link)  = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    println!("a next item = {:?}", a.tail());
+}
+
+
+/* 
+Another solution for avoiding reference cycles is reorganizing your data structures so that some references express ownership and some references don’t. 
+As a result, you can have cycles made up of some ownership relationships and some non-ownership relationships, and only the ownership relationships affect whether or not a value can be dropped
+*/
