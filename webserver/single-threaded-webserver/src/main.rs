71aceaa411511f51429bfe06369c5bd76a0a3efb
afaf-tech <fatihalfikri5@gmail.com>
2022-06-11 10:58:53 +0700

    feat: simulating slow request in single threaded webserver

--- webserver/single-threaded-webserver/src/main.rs
+++ webserver/single-threaded-webserver/src/main.rs
@@ -2,7 +2,8 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 use std::fs;
-
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -64,12 +65,18 @@ fn handle_connection_v2(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
+    // like define router in any other framework
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "hello.html")
+        ("HTTP/1.1 200 OK", "public/html/hello.html")
+    } else if buffer.starts_with(sleep) {
+        // when we try to access our server the time we execute this. then the server will wait until this process is finished
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "public/html/hello.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+        ("HTTP/1.1 404 NOT FOUND", "public/html/404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
